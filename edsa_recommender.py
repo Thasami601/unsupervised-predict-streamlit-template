9a195974f824b804fcdf0a935ebce3f40fc10953
Nkululeko <myeninkululeko27@gmail.com>
2020-07-22 20:59:32 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -104,7 +104,7 @@ def main():
         st.title("About The App")
         st.markdown("The application is about recomender systems. It recomends movies to a user based on two types of recomender system. Which are Collaborative-filtering and Content-Based filtering. Below is a brief explanation of the recomender systems")		
         st.subheader("Recommender Systems")
-        st.image("resources/imgs/image.PNG")
+        st.image("resources/imgs/image.png")
         st.markdown("**Collaborative-filtering:** In collaborative-filtering items are recommended, for example movies, based on how similar your user profile is to other users’, finds the users that are most similar to you and then recommends items that they have shown a preference for. This method suffers from the so-called cold-start problem: If there is a new movie, no-one else would’ve yet liked or watched it, so you’re not going to have this in your list of recommended movies, even if you’d love it.")
         st.markdown("**Content-based filtering:** This method uses attributes of the content to recommend similar content. It doesn’t have a cold-start problem because it works through attributes or tags of the content, such as actors, genres or directors, so that new movies can be recommended right away.")        
 		
